04cf306e6097fc2cf9b8c681fe1e5af1cca65d14
Suat Demir <suatdm@gmail.com>
2016-10-03 22:15:10 +0300

    #deleted all angular js files

--- src/main/webapp/app/components/form/pagination.constants.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('jhipsterReactMonolithicApp')
-        .constant('paginationConstants', {
-            'itemsPerPage': 20
-        });
-})();
